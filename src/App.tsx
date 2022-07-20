07e4887a74556a6f9ff42d7b0331b53733de51e0
sirjan tamang <sirjantmg99@gmail.com>
2022-07-20 22:33:15 +0545

    tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+	return (
+		<h1 className="text-3xl font-bold underline text-blue-500">
+			Hello world!
+			<button className="btn">primary</button>
+		</h1>
+	);
+}
